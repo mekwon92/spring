584139c3e225cbea9c681c88548d96ae6efaf499
mekwon92 <me92100984@gmail.com>
2024-12-10 11:03:25 +0900

    day2

--- demo/src/main/java/com/me92100984/demo/controller/MemberController.java
+++ demo/src/main/java/com/me92100984/demo/controller/MemberController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.me92100984.demo.service.MemberService;
 import com.me92100984.demo.vo.Member;
