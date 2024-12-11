d4d098bb95ee5764da0702ee71f84749ad4046e4
mekwon92 <me92100984@gmail.com>
2024-12-11 17:51:00 +0900

    advice

---
+++ aop/src/main/java/com/me92100984/aop/ex02/adv/Sourcing.java
@@ -0,0 +1,24 @@
+package com.me92100984.aop.ex02.adv;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+import lombok.extern.log4j.Log4j2;
+
+
+//around
+@Log4j2
+public class Sourcing implements MethodInterceptor {
+    
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        String source = invocation.getArguments()[0].toString();
+        log.info(source + " 양념을 만든다.");
+        Object o = invocation.proceed();//실제할일 처리
+
+        log.info(source + " 양념을 바른다.");
+        return o;
+    }
+    
+    
+}
